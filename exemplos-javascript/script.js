a004ebbe2df97d5275b07ef374339a059818ba0d
rafael-linux <rafaeldidier1009@gmail.com>
2024-04-03 15:49:26 -0300

    if/else

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,1 +1,16 @@
-console.log('Olá mundo!');
+let num1 = 100;
+let num2 = 100;
+let word = 'Palavra';
+let xablau = null;
+let babalu = undefined;
+
+if (num1 > num2) {
+  console.log('Num1 é o maior');
+} else if (num1 === num2) {
+  console.log('Empate');
+} else {
+  console.log('Num2 é o maior');
+};
+
+console.log(2 + 2 * 4 / 3);
+
