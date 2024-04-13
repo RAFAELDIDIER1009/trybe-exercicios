800311a5c594913eae168aef4d16407f8648676f
rafael-linux <rafaeldidier1009@gmail.com>
2024-04-13 09:59:15 -0300

    condição com ternário e ${

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,28 +1,6 @@
-let diaper;
-let babySise = 20;
+'condição' ? 'retorno de condição' : 'retorno se falsa'
 
+let temperature = 35
+let weather = temperature > 30 ? 'tá quente' : 'tá de boa';
 
-if (typeof babySise !== 'number') {
-  diaper = 'Valor inválido, digite um number';
-} else if(babySise <= 4) {
-  diaper = 'RN';
-} else if (babySise > 4 && babySise <= 6) {
-  diaper = 'RN+';
-}
- else if (babySise > 6 && babySise <= 8) {
-  diaper = 'PP';
-}
-else if (babySise > 8 && babySise <= 10) {
-  diaper = 'P';
-}
-else if (babySise > 10 && babySise <= 13) {
-  diaper = 'M';
-}
-else if (babySise > 13 && babySise <= 15) {
-  diaper = 'G';
-}
-else if (babySise > 15) {
-  diaper = 'GG';
-}
-
-console.log('o tamanho da fralda é: ' + diaper);
+console.log(`A temperatura é ${temperature}, portanto, ${weather}.`);
