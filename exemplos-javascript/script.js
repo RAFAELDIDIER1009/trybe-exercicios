d90813b58aef1c0bebdd94fb5860c5e570955328
rafael-linux <rafaeldidier1009@gmail.com>
2024-04-22 08:11:51 -0300

    Exercicio 3 JAVA

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,11 +1,15 @@
-// Exercicio 2
+// Exercicio 3
 
-let valor;
-let num = 20
+let num1 = 6;
+let num2 = 4;
+let num3 = 10;
 
-if (num > 10) {
-  valor = 'Xablau';
-} else (num < 10); {
-  valor = 'Zablou';
+if (num1 > num2 && num1 > num3) {
+  console.log('O maior número é:' + num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log('O maior número é o:' + num2);
+} else if (num3 > num1 && num3 > num2) {
+  console.log('O maior número é:' + num3);
+} else {
+  console.log("Vishh! Tem números iguais.");
 };
-console.log(valor);
