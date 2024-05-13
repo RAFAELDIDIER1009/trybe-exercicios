bbdca839398b30da406916d443bba9453dc2e841
rafael-linux <rafaeldidier1009@gmail.com>
2024-05-13 11:35:45 -0300

    questáo 02 array

--- Arrays-e-loop/arrays.js
+++ Arrays-e-loop/arrays.js
@@ -1,6 +1,9 @@
-// Exrcício 01 Arrays
- 
-const menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-const menuServices = (menu[Array.length]);
+// Exrcício 02 Arrays
 
-console.log(menuServices);
+const menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+const total = menu.push('Contato', 'Número');
+
+
+console.log(menu[Array.length -1]);
+console.log(total);
