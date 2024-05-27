646194ed8a5532eab5b4c022b0734f8391068933
rafael-linux <rafaeldidier1009@gmail.com>
2024-05-27 14:31:10 -0300

    string e math e random

--- Arrays-e-loop/loop-for.js
+++ Arrays-e-loop/loop-for.js
@@ -1,5 +1,8 @@
-const lotteryNumbers = [4, 8, 15, 16, 23, 42];
+const lotteryNumbers = [];
+const firstGame = [4, 8, 15, 16, 23, 42,];
 
-for (let index = 0; index < lotteryNumbers.length; index += 1) {
-console.log(lotteryNumbers[index] * 2);
-}
+for (let index = 0; index < 6; index += 1) {
+ lotteryNumbers.push(Math.ceil(Math.random() * 60));
+};
+
+console.log(lotteryNumbers);
