a9880d1be1593f79018b69204cacd718d2120c82
rafael-linux <rafaeldidier1009@gmail.com>
2024-06-04 09:03:44 -0300

    Somar os elementos de uma const com index e for

--- Arrays-e-loop/arrays.js
+++ Arrays-e-loop/arrays.js
@@ -1,6 +1,8 @@
-const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0
 
-for (let index = 0; index < groceryList.length; index += 1) {
-  const currentItem = groceryList[index];
-  console.log(currentItem);
+for (index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
 };
+
+console.log(result);
