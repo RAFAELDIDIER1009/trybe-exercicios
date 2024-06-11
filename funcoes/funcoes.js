de7eca600b2bfac33a5d27145db89981a9fcc6e9
rafael-linux <rafaeldidier1009@gmail.com>
2024-06-11 10:19:36 -0300

    Function and arrow function

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,1 +1,15 @@
-teste
+// Exemplo 1
+console.log(sum(10, 5));
+
+function sum (num1, num2) {
+return num1 + num2;
+}
+
+
+
+// Exemplo 2
+
+console.log(helloPeople('Rafa'));
+console.log(helloPeople('Pedro'));
+console.log(helloPeople('bob'));
+const helloPeople = (name, lastName) => `Olá ${name}, tudo bem?`;
