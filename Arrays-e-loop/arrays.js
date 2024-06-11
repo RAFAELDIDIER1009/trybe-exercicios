bdf3741590e3a660b78e2d951009ab3a626d2b70
rafael-linux <rafaeldidier1009@gmail.com>
2024-06-11 10:52:32 -0300

    Exercicio 01

--- Arrays-e-loop/arrays.js
+++ Arrays-e-loop/arrays.js
@@ -1,9 +1,18 @@
-let n = 4;
+let n = 10;
 let symbol = '*';
 let inputLine = '';
+let inputPosition = n - 1;
 
 for (let lineIndex = 0; lineIndex <= n; lineIndex += 1){
-  inputLine = inputLine + symbol;
+for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+  if (columnIndex < inputPosition) {
+    inputLine = inputLine + '';
+  } else {
+    inputLine = inputLine = symbol;
+  }
+};
   console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
 };
 
--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,15 +1,23 @@
-// Exemplo 1
-console.log(sum(10, 5));
+// Exercício: Crie uma função que retorne o maior e o maior número dentro de um array
 
-function sum (num1, num2) {
-return num1 + num2;
-}
+const biggestSmallerNumber = (array) => {
+  let bigger = array[0];
+  let smaller = array[0];
 
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] > bigger) {
+      bigger = array[index];
+    } else if (array[index] < smaller) {
+      smaller = array[index];
+    }
+  }
 
+  return `Menor: ${smaller}
+Maior ${bigger}`;
 
-// Exemplo 2
+};
+  
 
-console.log(helloPeople('Rafa'));
-console.log(helloPeople('Pedro'));
-console.log(helloPeople('bob'));
-const helloPeople = (name, lastName) => `Olá ${name}, tudo bem?`;
+console.log(biggestSmallerNumber ([1, 2, 3, 4, 6, -5, -4, 0,]));
+console.log(biggestSmallerNumber ([12, 23, 34, 44, 65, -500, -4, 0, 20, -10000]));
+
