9a5c5284cd6d218bd6a2ca93827446d3fbf6546c
rafael-linux <rafaeldidier1009@gmail.com>
2024-07-11 11:14:36 -0300

    o

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -1,2 +1,19 @@
 // informações do MacDonalds
 
+const macDonalds = {
+  bigMac: 26.90,
+  bigTasty: 39.00,
+  macFritasGrande: 11.90,
+  topSundayChocolate: 11.00,
+  isDelicious: true,
+address: {
+  city: 'São Paulo',
+  state: 'SP',
+  unitName: {
+    newShopping: 'Tatuapé',
+    oldShopping: 'GardenMol',
+  }
+}
+};
+
+console.log(Object.entries(macDonalds));
