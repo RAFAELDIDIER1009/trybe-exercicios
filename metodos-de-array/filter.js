9ea0133a78b8efce6cf0db503b433f481a9a9d1e
rafael-linux <rafaeldidier1009@gmail.com>
2024-09-20 10:27:14 -0300

    outra forma de escrita .filter .map

--- metodos-de-array/filter.js
+++ metodos-de-array/filter.js
@@ -11,10 +11,11 @@ const data = require('./data');
  */
 
 const getCardsByType = (data, cardType) => { //função do tipo de cada carta
- const FilterResult = data.filter((card) => card.typeId === cardType);  //filtro de todas as cartas id 1
-const mapResult = FilterResult.map((card) => ({ typeId: card.typeId, name: card.name})); //obj criado com map alterando o array
+ const FilterResult = data
+ .filter((card) => card.typeId === cardType) //filtro criado novo array
+ .map((card) => ({ typeId: card.typeId, name: card.name})); // o resultado ja ligaod com o map
 
- return mapResult; // imprimindo o novo array com obj
+ return FilterResult; 
 }
 
 console.log(getCardsByType(data.cards, 3)); // log com parametros da função
