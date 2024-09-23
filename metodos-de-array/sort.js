55fda5c48623e590c35a0259ca17dbf5c55d16dd
rafael-linux <rafaeldidier1009@gmail.com>
2024-09-23 08:11:16 -0300

    aprendendo sort

---
+++ metodos-de-array/sort.js
@@ -0,0 +1,7 @@
+const array = [1, 6, 3, 2, 9, 8, 5, 12, 33, 4, 7, 0]; // array de numeros
+
+const sortArray = array.sort((a, b) => b - a); // ordenar com numeros de dois algoritimos: crescente a - b e decrescente b - a
+
+console.log(sortArray);
+
+
